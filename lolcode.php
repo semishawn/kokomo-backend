552f999b3ddb902d356e7963734b22199ad16f07
semishawn <45698497+semishawn@users.noreply.github.com>
2021-10-05 03:35:31 -0400

    first true compile test with ajax

---
+++ lolcode.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+ini_set("display_errors", 1); 
+error_reporting(E_ALL);
+
+$lolcode = $_REQUEST["lolcode"];
+$lolFileName = "script.lol";
+
+$lolFile = fopen($lolFileName, "w");
+fwrite($lolFile, $lolcode);
+$output = shell_exec("lci " . $lolFileName);
+fclose($lolFile);
+unlink($lolFileName);
+
+echo $output;
+?>
